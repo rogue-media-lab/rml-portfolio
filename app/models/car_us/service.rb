a20b6e639e057412c5629c8175d282189272896e
Mason Roberts <mrobe18@gmail.com>
2026-07-08 19:11:32 -0400

    fix: add active scope to CarUs::Service model

--- app/models/car_us/service.rb
+++ app/models/car_us/service.rb
@@ -3,4 +3,6 @@ class CarUs::Service < ApplicationRecord
 
   validates :name, presence: true
   validates :duration_minutes, numericality: { greater_than: 0 }, allow_nil: true
+
+  scope :active, -> { where(active: true) }
 end
