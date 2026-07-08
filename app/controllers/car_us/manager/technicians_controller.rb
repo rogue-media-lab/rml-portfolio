1eb080c3332878cef171d99f8570ad070bb20549
Mason Roberts <mrobe18@gmail.com>
2026-07-08 18:59:34 -0400

    feat: add edit/update actions for technician targets

--- app/controllers/car_us/manager/technicians_controller.rb
+++ app/controllers/car_us/manager/technicians_controller.rb
@@ -18,6 +18,21 @@ module CarUs
         end
       end
 
+      def edit
+        @technician = current_shop.technicians.find(params[:id])
+        @shop_services = current_shop.services.active.order(:name)
+      end
+
+      def update
+        @technician = current_shop.technicians.find(params[:id])
+        if @technician.update(technician_update_params)
+          redirect_to manager_technicians_path, notice: "#{@technician.email} targets updated."
+        else
+          @shop_services = current_shop.services.active.order(:name)
+          render :edit, status: :unprocessable_entity
+        end
+      end
+
       def destroy
         tech = current_shop.technicians.find(params[:id])
         if tech == current_technician
@@ -32,6 +47,14 @@ module CarUs
 
       def technician_params
         params.require(:technician).permit(:email, :password, :password_confirmation)
+      end
+
+      def technician_update_params
+        params.require(:technician).permit(
+          :email, :password, :password_confirmation,
+          :preferred_hours, :target_hours,
+          target_service_ids: []
+        )
       end
     end
   end
